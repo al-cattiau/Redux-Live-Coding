9f727b6b9b40b5bc33f7d9aac0225bfe32d4e2d0
md2 <md2@bellcode.com>
2018-10-31 14:51:41 +0800

    准备分享，把代码删除到剩下骨架

--- example.js
+++ example.js
@@ -20,80 +20,24 @@ store = {
 通过 addOneCount 让 store 中 count 的值加一
 `;
 // action
-const addOneCountString = "ADD_ONE_COUNT";
+// const addOneCountString
 
 // action creator
-const addOneCount = () => ({
-  type: addOneCountString,
-  payload: null
-});
+// const addOneCount
 
-// thunk action creator
-const addOneCountThunk = () => (getState, dispatch) => {
-  setTimeout(() => {
-    dispatch({
-      type: addOneCountString,
-      payload: null
-    });
-  }, 1000);
-};
 
 // reducer
-function counter(
-  state = {
-    count: 0
-  },
-  action
-) {
-  switch (action.type) {
-    case addOneCountString:
-      return { count: state.count + 1 };
-    default:
-      return state;
-  }
-}
+// function counter() {
+// }
 
-const consoleActionMiddleWare1 = ({ getState, dispatch }) => next => action => {
-  console.log("log action in middleware1", action);
-  next(action);
-};
 
-const consoleActionMiddleWare2 = ({ getState, dispatch }) => next => action => {
-  console.log("log action in middleware2", action);
-  next(action);
-};
 
-const fakeReduxThunk = ({ getState, dispatch }) => next => action => {
-  if (typeof action === "function") {
-    action(getState, dispatch);
-  } else {
-    next(action);
-  }
-};
-
-let repeatTime = 3;
-const repeatThroughMiddleWare = store => next => action => {
-  console.log("repeatTime", repeatTime);
-  if (repeatTime > 0) {
-    repeatTime -= 1;
-    store.dispatch(action);
-  } else {
-    next(action);
-  }
-};
 
 // store
-let store = createStore(counter, [
-  consoleActionMiddleWare1,
-  consoleActionMiddleWare2,
-  fakeReduxThunk
-]);
+// let store = createStore();
 
-// let store = createStore(counter, [consoleActionMiddleWare1, consoleActionMiddleWare2] );
 
-// console.log('inital state', store.getState());
+// store.subscribe
 
-store.subscribe(a => console.log("state updated", store.getState()));
-
-store.dispatch(addOneCountThunk());
-store.dispatch(addOneCount());
+// store.dispatch
+// store.dispatch
